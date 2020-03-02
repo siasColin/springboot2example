5c715df9e979b304620b091ef359b1ac7e21e65f
siasColin <1540247870@qq.com>
2020-03-03 00:00:10 +0800

    注释掉测试定时任务

--- src/main/java/cn/net/colin/task/test/TestScheduled.java
+++ src/main/java/cn/net/colin/task/test/TestScheduled.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-@Component
+//@Component
 public class TestScheduled {
     Logger logger = LoggerFactory.getLogger(TestScheduled.class);
 
