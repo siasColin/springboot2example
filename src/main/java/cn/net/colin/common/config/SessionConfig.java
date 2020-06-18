d8221283402bb1dff06bb024f16bd795f7666d54
siasColin <1540247870@qq.com>
2020-06-18 14:37:04 +0800

    整合springsession

---
+++ src/main/java/cn/net/colin/common/config/SessionConfig.java
@@ -0,0 +1,16 @@
+package cn.net.colin.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+
+/**
+ * @Package: cn.net.colin.common.config
+ * @Author: sxf
+ * @Date: 2020-6-18
+ * @Description: spirngsession 配置类,结合redis可实现多模块建session共享
+ */
+//@Configuration
+//启用ResisSession存储
+//@EnableRedisHttpSession(maxInactiveIntervalInSeconds= 1800)
+public class SessionConfig {
+}
